d5623e3663dc48c03c2d58dc9f94094e2a1abf99
liujunchao <liujunchao@pengwin.com>
2024-09-29 18:19:26 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -369,8 +369,9 @@ router.post("/fileToUrl", async (ctx, next) => {
     headers: {
       "Content-Type": "application/json; charset=utf-8",
       "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
       Accept: "*/*",
+      cookie: "PHPSESSID=gs8htu7erl4800gaku1f97ervc",
     },
     responseType: "arraybuffer", // 设Y置响应类型为 arraybuffer
   });
